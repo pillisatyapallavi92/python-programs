7e56c9acbb1e3f9ae3c80f017fe8988afa9ac530
Pilli Satya Pallavi <pillisatyapallavi92@gmail.com>
2026-08-17 20:11:52 +0530

    Add student program

---
+++ student.py
@@ -0,0 +1,25 @@
+
+# typecasting to int
+num_int = int(5.7)
+print(num_int)
+print(type(num_int))
+
+
+num_float=float("3.14")
+print(num_float)
+print(type(num_float))
+
+
+
+message=str(42)
+print(message)
+print(type(message))
+
+
+is_true=bool(1)
+is_false=bool(0)
+print(is_true)
+print(type(is_true))
+
+print(is_false)
+print(type(is_false))
